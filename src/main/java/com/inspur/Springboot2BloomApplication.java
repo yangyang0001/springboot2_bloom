4db5f505edbfc7e753593da5a08c99f4f77e1445
yangjianwei <2017585616@qq.com>
2019-07-29 05:00:03 +0800

    ComplateableFutrue

--- src/main/java/com/inspur/Springboot2BloomApplication.java
+++ src/main/java/com/inspur/Springboot2BloomApplication.java
@@ -3,10 +3,12 @@ package com.inspur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 @SpringBootApplication
+@EnableScheduling
 public class Springboot2BloomApplication {
 
 	public static void main(String[] args) {
